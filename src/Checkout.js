46167d443a1b4d735749f62c0d7d5afe994db372
badrinath117 <70693954+badrinath117@users.noreply.github.com>
2022-02-07 03:22:15 +0530

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,7 +10,7 @@ function Checkout() {
             <div className="checkout__left">
                <img 
                className="checkout__ad"
-               src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+               src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""
                alt="">
 
                </img>
